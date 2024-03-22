852488fd3809087c3d3205479fb5039c4eecf55e
Giovanni Fernandes <gfmsantos22@gmail.com>
2024-03-21 22:25:16 -0300

    2103

--- crud_mysqli/form.php
+++ crud_mysqli/form.php
@@ -4,11 +4,63 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>CRUD - MSQLI</title>
+    <title>Novo cadastro</title>
 </head>
 
 <body>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col">
+                <form action="salvar.php" method="post">
+                    <div class="card w-50">
+                        <div class="card-header fw-bold fs-3">
+                            <i class="bi bi-person-fill me-3"></i>Cadastro de cliente
+                        </div>
+                        <div class="card-body d-flex ">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <label for="pk_cliente" class="form-label">ID:</label>
+                                        <input readonly type="number" name="pk_cliente" id="pk_cliente" class="form-control ">
+                                    </div>
+                                    <div class="col-md-10">
+                                        <label for="nome" class="form-label">Cliente:</label>
+                                        <input type="text" name="nome" id="nome" class="form-control">
+                                    </div>
+
+                                </div>
+                                <div class="row mt-3">
+                                    <div class="col-md-4">
+                                        <label for="cpf" class="form-label">CPF:</label>
+                                        <input type="text" name="cpf" id="cpf" class="form-control" data-mask="000.000.000-00">
+                                    </div>
+                                    <div class="col-md-4">
+                                        <label for="tel" class="form-label">Whatsapp:</label>
+                                        <input type="text" name="tel" id="tel" class="form-control" data-mask="(00) 00000-0000">
+                                    </div>
+                                    <div class="col-md-4">
+                                        <label for="email" class="form-label">Email:</label>
+                                        <input type="email" name="email" id="email" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="card-footer text-end ">
+                            <button class="btn btn-outline-danger" type="button">
+                                <i class="bi bi-x"></i> Cancelar
+                            </button>
+                            <button class="btn btn-success" type="submit">
+                                <i class="bi bi-check-all"></i> Enviar
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 
 
@@ -18,8 +70,9 @@
 
 
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 
 </html>
--- crud_mysqli/index.php
+++ crud_mysqli/index.php
@@ -5,20 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>CRUD - MSQLI</title>
 </head>
 
 <body>
-    <div class="container">
+    <div class="container mt-3">
         <div class="row">
             <div class="col">
                 <div class="card">
                     <div class="card-header">
                         Lista de ordem de serviço
+                        <button class="btn btn-primary float-end">
+                        Adicionar <i class="i bi-plus"></i>
+                        </button>
                     </div>
-                    <div class="card-body">
-                        <table class="table table-hover ">
-                            <thead>
+                    <div class="card-body ">
+                        <table class="table table-hover  table-responsive align-middle">
+                            <thead class="table-secondary">
                                 <tr>
                                     <th>ID</th>
                                     <th>Cliente</th>
@@ -33,14 +37,20 @@
                                     <td>Giovanni</td>
                                     <td>Manutenção</td>
                                     <td>R$150</td>
-                                    <td>Botões</td>
+                                    <td>
+                                        <button class="btn btn-info btn-sm "><i class="bi bi-pencil-square"></i></button>
+                                        <button class="btn btn-danger btn-sm "><i class="bi bi-trash"></i></button>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>2</td>
                                     <td>otra pessoa</td>
                                     <td>algo</td>
                                     <td>R$8000</td>
-                                    <td>Botões</td>
+                                    <td>
+                                        <button class="btn btn-info btn-sm "><i class="bi bi-pencil-square"></i></button>
+                                        <button class="btn btn-danger btn-sm "><i class="bi bi-trash"></i></button>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
